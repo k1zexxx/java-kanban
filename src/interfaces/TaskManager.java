190351c59ef6f9a3c40794cf326890f2b9c59aa0
k1zexxx <kolyangavr@mail.ru>
2025-07-28 14:45:36 +0300

    Исправил TaskManager

--- src/interfaces/TaskManager.java
+++ src/interfaces/TaskManager.java
@@ -8,23 +8,42 @@ import java.util.HashMap;
 
 public interface TaskManager<T> {
     HashMap<Integer, T> getTasks();
+
     HashMap<Integer, T> getEpicTasks();
+
     HashMap<Integer, T> getSubTasks();
+
     T getTasksId(int taskId);
+
     T getEpicTasksId(int taskId);
+
     T getSubTasksId(int taskId);
+
     void deleteTask();
+
     void deleteEpicTask();
+
     void deleteSubTask();
+
     void deleteTaskId(int id);
+
     void deleteEpicTaskId(int id);
+
     void deleteSubTaskId(int id);
+
     void createTask(Task task);
+
     void createEpicTask(EpicTask epicTask);
+
     void createSubTask(SubTask subTask);
+
     void updateTask(Task task);
+
     void updateEpicTask(EpicTask epicTask);
+
     void updateSubTask(SubTask subTask);
+
     ArrayList<SubTask> getSubTaskList(int epicId);
+    
     static void printAllTasks(TaskManager<Task> manager){}
 }
